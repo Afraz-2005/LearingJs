5890cf99b3ae6ff68e77d8f0db801e7cd52f1209
Imam MAhbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2021-08-22 20:29:18 +0600

    updated

---
+++ challenge1/static/scripts/script.js
@@ -0,0 +1,32 @@
+//Selectors and listeners
+document.querySelector(".btn-1").addEventListener('click', ageInDays);
+document.querySelector(".btn-2").addEventListener('click', reset);
+
+//global variables
+let date = new Date();
+let year = date.getFullYear();
+let birthYear;
+let textAnswer;
+
+
+function ageInDays(){
+    birthYear = prompt("Enter your birth year: ");
+    let age_in_days = (year - birthYear) * 365;
+    textAnswer = `You are ${age_in_days} days old`;
+    checkNegative();
+    document.getElementById("result").innerHTML = textAnswer;
+    console.log(age_in_days);
+}
+
+function reset(){
+    document.location.reload();
+}
+
+function checkNegative(){
+    if (birthYear > year) {
+        textAnswer = `Invalid input! it's ${year} now`;
+    }
+    if (birtYear == "") {
+        console.log('Empty input from user');
+    }
+}
